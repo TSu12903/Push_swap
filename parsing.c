5e2ade9d47011d8e0ed6399f7d82924ff2b896c4
Timothy Cybak <tcybak@2F5.42angouleme.fr>
2024-12-02 19:01:15 +0100

    up_need_fix_error_valgrind

--- parsing.c
+++ parsing.c
@@ -11,8 +11,9 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
-static int	ft_check_sign_num(char *str)
+int	ft_check_sign_num(char *str)
 {
 	int	i;
 
@@ -26,27 +27,70 @@ static int	ft_check_sign_num(char *str)
 			&& !(str[i + 1] >= '0' && str[i + 1] <= '9'))
 			return (0);
 		if ((str[i] >= '0' && str[i] <= '9')
-			&& !(str[i + 1] == ' ' || str[i + 1] == '\0'))
+			&& !(str[i + 1] == ' ' || str[i + 1] == '\0'
+				|| (str[i] >= '0' && str[i] <= '9')))
 			return (0);
 		i++;
 	}
-	return (1);
+	return (i);
+}
+
+int	ft_verfi_twice(int *str, int num)
+{
+	int	j;
+	int	i;
+
+	i = 0;
+	while (i < num)
+	{
+		j = 1;
+		while (j < i)
+		{
+			if (str[i] == str[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+int	total_word(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] == ' ' || str[i] == '+' || str[i] == '-') 
+			i++;
+		while (str[i] >= '0' && str[i] <= '9')
+			i++; 
+		count++;
+	}
+	return (count);
 }
 
 int	main(int ac, char **av)
 {
 	int		i;
-	int		str;
+	int		*str;
 	int		j;
-	char	**tab;
+	char		**tab;
+	int		nb_count;
+	int		k;
 
 	if (ac == 1)
 		return (0);
 	i = 1;
+	j = 0;
 	while (av[i])
 	{
-		str = ft_check_sign_num(av[i]);
-		if (str == 0)
+		j = ft_check_sign_num(av[i]);
+		
+		if (j == 0)
 		{
 			ft_printf("%s", "Error");
 			return (0);
@@ -54,17 +98,43 @@ int	main(int ac, char **av)
 		i++;
 	}
 	i = 1;
+	nb_count = 0;
+	while (av[i])
+	{
+		nb_count += total_word(av[i]);
+		i++;
+	}
+	str = malloc(sizeof(int) * nb_count);
+	if (str == 0)
+		return (0);
+	i = 1;
+	k = 0;
 	while (av[i])
 	{
 		tab = ft_split(av[i], ' ');
 		j = 0;
-		while(tab[j])
+		while (tab[j])
 		{
-			ft_printf("%s\n", tab[j]);
+			str[k] = ft_atoi(tab[j]);
 			free(tab[j]);
+			k++;
 			j++;
 		}
 		free(tab);
 		i++;
 	}
+	j = ft_verfi_twice(str, nb_count);
+	if (j == 0)
+	{
+		ft_printf("%s", "Error");
+		free(str);
+		return (0);
+	}
+	j = 0;
+	while (j <= nb_count - 1)
+	{
+		ft_printf("%d\n", str[j]);
+		j++;
+	}
+	free(str);
 }
