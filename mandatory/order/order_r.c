2f65ecea831da7b570b4abc9589f79af36770528
TSu12903 <timcybak@gmail.com>
2024-12-15 01:17:15 +0100

    norm order_r

--- mandatory/order/order_r.c
+++ mandatory/order/order_r.c
@@ -6,38 +6,38 @@
 /*   By: tsugeku <tsugeku@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 00:47:30 by tsugeku           #+#    #+#             */
-/*   Updated: 2024/12/15 01:13:59 by tsugeku          ###   ########.fr       */
+/*   Updated: 2024/12/15 01:16:54 by tsugeku          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void    ft_rotate_a(t_stack *stack, t_init *init)
+void	ft_rotate_a(t_stack *stack, t_init *init)
 {
-    init->k = stack->stack_a[0];
-    init->i = 1;
-    init->j = 0;
-    stack->size_a = ft_strlen_long(stack->stack_a);
-    while(init->i <= stack->size_a - 1)
-    {
-        stack->stack_a[init->j] = stack->stack_a[init->i];
-        init->i++;
-        init->j++;
-    }
-    stack->stack_a[stack->size_a - 1] = init->k;
+	init->k = stack->stack_a[0];
+	init->i = 1;
+	init->j = 0;
+	stack->size_a = ft_strlen_long(stack->stack_a);
+	while (init->i <= stack->size_a - 1)
+	{
+		stack->stack_a[init->j] = stack->stack_a[init->i];
+		init->i++;
+		init->j++;
+	}
+	stack->stack_a[stack->size_a - 1] = init->k;
 }
 
-void    ft_rotate_b(t_stack *stack, t_init *init)
+void	ft_rotate_b(t_stack *stack, t_init *init)
 {
-    init->k = stack->stack_b[0];
-    init->i = 1;
-    init->j = 0;
-    stack->size_b = ft_strlen_long(stack->stack_b);
-    while(init->i <= stack->size_b - 1)
-    {
-        stack->stack_b[init->j] = stack->stack_b[init->i];
-        init->i++;
-        init->j++;
-    }
-    stack->stack_b[stack->size_b - 1] = init->k;
+	init->k = stack->stack_b[0];
+	init->i = 1;
+	init->j = 0;
+	stack->size_b = ft_strlen_long(stack->stack_b);
+	while (init->i <= stack->size_b - 1)
+	{
+		stack->stack_b[init->j] = stack->stack_b[init->i];
+		init->i++;
+		init->j++;
+	}
+	stack->stack_b[stack->size_b - 1] = init->k;
 }
