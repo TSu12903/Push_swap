e1d2a5ddc63335d3c7dbf453b8b27c4828fc43d9
Timothy Cybak <tcybak@1A5.42angouleme.fr>
2025-01-03 20:40:52 +0100

    up norm less_tree

--- mandatory/algo/algo_best_move.c
+++ mandatory/algo/algo_best_move.c
@@ -6,7 +6,7 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 19:44:21 by tcybak            #+#    #+#             */
-/*   Updated: 2025/01/03 20:38:55 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/01/03 20:40:27 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@ void	ft_less_tree(t_stack *stack, t_init *init)
 	init->tree = 0;
 	if (stack->size_a == 3)
 	{
-		if (stack->stack_a[init->tree] > stack->stack_a[init->tree + 1] 
+		if (stack->stack_a[init->tree] > stack->stack_a[init->tree + 1]
 			&& stack->stack_a[init->tree] < stack->stack_a[init->tree + 2])
 			ft_swap_a(stack, init);
-		else if (stack->stack_a[init->tree] > stack->stack_a[init->tree + 2] 
+		else if (stack->stack_a[init->tree] > stack->stack_a[init->tree + 2]
 			&& stack->stack_a[init->tree + 2] > stack->stack_a[init->tree + 1])
 			ft_rotate_a(stack, *init);
-		else if (stack->stack_a[init->tree] > stack->stack_a[init->tree + 1] 
+		else if (stack->stack_a[init->tree] > stack->stack_a[init->tree + 1]
 			&& stack->stack_a[init->tree + 1] > stack->stack_a[init->tree + 2])
 		{
 			ft_swap_a(stack, init);
 			ft_reverse_rotate_a(stack, *init);
 		}
-		else if (stack->stack_a[init->tree + 1] > stack->stack_a[init->tree + 2] 
+		else if (stack->stack_a[init->tree + 1] > stack->stack_a[init->tree + 2]
 			&& stack->stack_a[init->tree + 2] > stack->stack_a[init->tree])
 		{
 			ft_reverse_rotate_a(stack, *init);
 			ft_swap_a(stack, init);
 		}
-		else if (stack->stack_a[init->tree + 1] > stack->stack_a[init->tree] 
+		else if (stack->stack_a[init->tree + 1] > stack->stack_a[init->tree]
 			&& stack->stack_a[init->tree] > stack->stack_a[init->tree + 2])
 			ft_reverse_rotate_a(stack, *init);
 	}
